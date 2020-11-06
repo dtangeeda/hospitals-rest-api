2e0f7f89161af01669a74342c6c179fd331c0394
zeeshanbarcha <zeeshanbarcha554@gmail.com>
2020-11-06 19:17:59 +0500

    Adding cors

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 global.__basedir = __dirname;
-require('dotenv/config');
-const express = require('express');
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3005;
-const { createRoutes } = require( './router' );
 
-createRoutes( app );
+app.use(cors());
+const { createRoutes } = require("./router");
+
+createRoutes(app);
 console.log("Loading the server.js file for the REST API");
 
 //Start Server for REST API
-app.listen( PORT, () => console.log( `REST Server has started on port ${PORT}` ) );
+app.listen(PORT, () => console.log(`REST Server has started on port ${PORT}`));
 module.exports = app;
